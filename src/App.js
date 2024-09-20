6414c6a50eb667cd8e93198de77076c49c5a765a
Muqadas Rahoojo <muqadasrahoojo83@gmail.com>
2024-09-20 12:13:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  
+
+ console.log('app comp');
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={''} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
